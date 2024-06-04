0caa7abc3b9ceb18a50b28df4b30d5f56cb756e2
Christiane <christianevillaroel@gmail.com>
2024-06-04 13:19:17 -0400

    Recursion CPP File

--- Final Exam/Recursion.cpp
+++ Final Exam/Recursion.cpp
@@ -2,9 +2,27 @@
 using namespace std;
 int sum(int x);
 int f(int x);
+
 int main (){
     int number=5;
-    cout<<f(number);
+    string str = "car";
+    int i = 0; int j = str.length()-1;
+
+    string strCopy= str;
+    cout<<strCopy<<endl;
+    while (i < str.length())
+    {
+        if(str[i]==str[j]){
+           strCopy= strCopy.substr(1,(strCopy.length()-2));
+           cout<<strCopy<<endl;
+        }
+        else
+            return false;
+    
+        i++;
+        j--;
+    }
+    
     return 0;
 }
 int sum(int x){
